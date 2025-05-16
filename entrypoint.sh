f29cc18cde15c74f511f95359d07d2fa308849d5
Ibrahim Bello <119727626+Ibrocold@users.noreply.github.com>
2025-05-16 02:16:50 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ until mysqladmin ping --silent; do
 done
 
 source venv/bin/activate   # ✅ Activates the virtual environment
-nohup python3 run.py > app.log 2>&1 & # ✅ Runs the app in the background and keeps it open
+python3 run.py  # ✅ Runs the app in the background and keeps it open
